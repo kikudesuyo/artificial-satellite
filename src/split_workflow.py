7f38bcf86370bb640fe5e093a50dce8701f4d545
Ishida-Yuichiro <110882936+Ishida-Yuichiro@users.noreply.github.com>
2023-08-29 14:34:25 +0900

    edit pathes of split_workflow.py

--- src/split_workflow.py
+++ src/split_workflow.py
@@ -2,10 +2,11 @@ from format.uart_communication import receive_command, send_command
 from util import shutdown
 from analysis.file_generation import convert_img_into_text, split_text_string
 
-send_command("起動完了")
-receive_command("分割指示")
-convert_img_into_text("/img/downlink_img/compressed_img.jpg")
-split_text_string("/data/downlink_data.txt")
-#アップリンクの返答　必要？
-send_command("シャットダウン要求")
-shutdown()
+def split_workflow():
+    send_command("起動完了")
+    receive_command("分割指示")
+    convert_img_into_text("/img/downlink_img/compressed_img.jpg")
+    split_text_string("/data/downlink_data.txt")
+    #アップリンクの返答　必要？
+    send_command("シャットダウン要求")
+    shutdown()
