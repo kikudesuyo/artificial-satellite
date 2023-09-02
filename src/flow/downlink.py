34933dd930d10ba8e7f504780b5f9d4cbb18b470
i-spes <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-09-02 14:52:55 +0900

    edit downlink.py to add the communication_main and send_command of the end of the communication

--- src/flow/downlink.py
+++ src/flow/downlink.py
@@ -1,12 +1,14 @@
 from downlink.shape_up import get_aurora_data, get_splited_data
 from format.uart_communication import send_command, receive_command
+from downlink.main import main as communication_main
 
 def downlink_flow():
   if "オーロラデータ送信":
-    aurora_data = get_aurora_data("/data/aurora_data/*")
+    sending_data = get_aurora_data("/data/aurora_data/*")
   elif "画像データ送信":
-    aurora_img = get_splited_data("/data/aurora_img/*.txt")
+    sending_data = get_splited_data("/data/aurora_img/*.txt")
   elif "画像データ番号指定":
-    aurora_img = get_splited_data("/data/aurora_data/*.txt")
-  
+    sending_data = get_splited_data("/data/aurora_data/*.txt")
+  communication_main(sending_data)
+  send_command("通信終了")
   
