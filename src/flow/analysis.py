008e4f95202d4f3595e97192c593043579b119cb
i-spes <icswse@gmail.com>
2023-09-02 16:01:14 +0900

    edit analysis.py and add delete command when the number of the files in the aurora_data is bigger than 1000

--- src/flow/analysis.py
+++ src/flow/analysis.py
@@ -1,9 +1,13 @@
 from format.uart_communication import receive_command, send_command
 from analysis.main import main as analysis_main
-from util import shutdown
+from util import shutdown, delete_files
+from helper.file_operation import get_amount
 
 def analysis_flow():
   send_command("解析指示を受けました")
+  amount_aurora_data = get_amount("/data/aurora_data")
+  if amount_aurora_data >= 1000:
+    delete_files("/data/aurora_data")
   analysis_main()
   send_command("電力停止要求")
   receive_command("シャットダウン要求受信")
--- src/flow/downlink.py
+++ src/flow/downlink.py
@@ -1,14 +1,18 @@
 from downlink.shape_up import get_aurora_data, get_splited_data
 from format.uart_communication import send_command, receive_command
 from downlink.main import main as communication_main
+from util import delete_files
 
 def downlink_flow():
   if "オーロラデータ送信":
     sending_data = get_aurora_data("/data/aurora_data/*")
+    communication_main(sending_data)
+    delete_files("/data/aurora_data")
   elif "画像データ送信":
     sending_data = get_splited_data("/data/aurora_img/*.txt")
+    communication_main(sending_data)
   elif "画像データ番号指定":
     sending_data = get_splited_data("/data/aurora_data/*.txt")
-  communication_main(sending_data)
+    communication_main(sending_data)
   send_command("通信終了")
   
