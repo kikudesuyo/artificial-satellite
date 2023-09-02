756ea6741d3fa4c3e7395b5ba010dfdc9cf439c5
i-spes <icswse@gmail.com>
2023-09-02 16:12:40 +0900

    add downlink_img in img to save the img for the downlink

--- src/flow/split.py
+++ src/flow/split.py
@@ -1,5 +1,5 @@
 from format.uart_communication import receive_command, send_command
-from util import shutdown
+from util import shutdown, delete_files
 from analysis.file_generation import convert_img_into_text, split_text_string
 
 def split_flow():
@@ -7,6 +7,8 @@ def split_flow():
     receive_command("分割指示")
     convert_img_into_text("/img/downlink_img/compressed_img.jpg")
     split_text_string("/data/downlink_data.txt")
+    delete_files("/img/downlink_img")
+
     #アップリンクの返答　必要？
     send_command("シャットダウン要求")
     shutdown()
