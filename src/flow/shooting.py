bb47c5a67e0ccd53297c07d908ce4b81be64c623
i-spes <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-09-02 16:20:13 +0900

    edit shooting.py to delete images when the number of the images in shooting_img is bigger than 2000, which is the number of images the satellite will take in the three times

--- src/flow/shooting.py
+++ src/flow/shooting.py
@@ -1,6 +1,7 @@
 from shooting.take_photograph import take_photo
 from format.uart_communication import receive_command, send_command
-from util import shutdown, set_date_on_raspi
+from util import shutdown, set_date_on_raspi, delete_files
+from helper.file_operation import get_amount
 
 
 def shooting_flow():
@@ -8,6 +9,9 @@ def shooting_flow():
   row_date = receive_command()
   date = hex(row_date)
   set_date_on_raspi(date)
+  amount_img = get_amount("/img/shooting_img")
+  if amount_img >= 2000:
+    delete_files("/img/shooting_img")
   take_photo(750, 2000) #緯度によって強制停止してもいいかもしれない（別のターミナルで）
   send_command("継続可能かEPSに尋ねる")
   command = receive_command("継続可能かどうか")
