2f2388e2176e5981d871b230f29e4c4c5cc389fb
i-spes <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-09-02 14:32:13 +0900

    edit analysis_flow.py to change the name of the defined function to analysis_flow

--- src/flow/analysis.py
+++ src/flow/analysis.py
@@ -2,7 +2,7 @@ from format.uart_communication import receive_command, send_command
 from analysis.main import main as analysis_main
 from util import shutdown
 
-def alanasis_flow():
+def analysis_flow():
   send_command("解析指示を受けました")
   analysis_main()
   send_command("電力停止要求")
