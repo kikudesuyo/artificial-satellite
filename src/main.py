14a0ff97a429ad84b76a6c0956e2c4912a8a4e27
i-spes <icswse@gmail.com>
2023-09-02 14:43:13 +0900

    edit main.py to add the detail of the if sentenses

--- src/main.py
+++ src/main.py
@@ -8,24 +8,15 @@ def execute():
   command = receive_command()
   
   if command == "オーロラ撮影":
-    """撮影フローに従う"""
     shooting_flow()
     after_shooting = "撮影処理"
   elif command == "画像解析" or after_shooting == "撮影後継続可能":
-    """解析フローに従う"""
     analysis_flow()
   elif command == "画像分割":
     delete_files("/data/aurora_img")
-    convert_img_into_text("/img/downlink_img/compressed_img.jpg")
-    split_text_string("/data/downlink_data.txt")
-    send_command("分割終了")
+    split_flow()
   elif command == "ダウンリンク":
-    downlink_command = receive_command(15)
-    if downlink_command == "オーロラデータダウンリンク":
-      sending_data = get_aurora_data("/data/aurora_data/*.txt")
-    elif downlink_command == "画像分割ダウンリンク":
-      #indexの指定をどうするかを考える(オーロラデータは行わない予定なのでそこに注意)
-      sending_data = get_splited_data("/data/aurora_img/*.txt")
+    downlink_flow()
     communication_main(sending_data)
     send_command("通信終了")
     delete_files("/data/aurora_data")
