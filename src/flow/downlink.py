2bccabeb3d8600025a0b3e6220660351b42b68f3
i-spes <icswse@gmail.com>
2023-09-03 13:37:39 +0900

    downlink.py

--- src/flow/downlink.py
+++ src/flow/downlink.py
@@ -1,18 +1,20 @@
 from downlink.shape_up import get_aurora_data, get_splited_data
 from format.uart_communication import send_command, receive_command
-from downlink.main import main as communication_main
+#from downlink.main import main as communication_main
+from format.format import send_data as communication_main
 from util import delete_files
+from constant import COM_ADDR
 
 def downlink_flow():
   if "オーロラデータ送信":
     sending_data = get_aurora_data("/data/aurora_data/*")
-    communication_main(sending_data)
+    communication_main(COM_ADDR, sending_data)
     delete_files("/data/aurora_data")
   elif "画像データ送信":
     sending_data = get_splited_data("/data/aurora_img/*.txt")
-    communication_main(sending_data)
+    communication_main(COM_ADDR, sending_data)
   elif "画像データ番号指定":
     sending_data = get_splited_data("/data/aurora_img/*.txt")
-    communication_main(sending_data)
+    communication_main(COM_ADDR, sending_data)
   elif "画像データ送信完了":
-    delete_files("/data/aurora_img")  
+    delete_files("/data/aurora_img")
