7232911be7531957f8308372efbf5c8712a9fcfe
i-spes <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-10-03 18:05:49 +0900

    add output_raspi_status in split flow

--- src/flow/split.py
+++ src/flow/split.py
@@ -1,8 +1,12 @@
 from util import delete_files
+from helper.file_operation import output_raspi_status
 from analysis.file_generation import convert_img_into_text, split_text_string
+from constant.status import SPLIT_INTERRUPTION, OTHERS_COMPLETION
 
 def split_flow():
+    output_raspi_status(SPLIT_INTERRUPTION)
     delete_files("/data/aurora_img")
     convert_img_into_text("/img/downlink_img/compressed_img.jpg")
     split_text_string("/data/downlink_data.txt")
     #delete_files("/img/downlink_img")
+    output_raspi_status(OTHERS_COMPLETION)
--- src/sandbox.py
+++
@@ -1,6 +0,0 @@
-from flow.downlink import get_downlink_data
-from downlink.downlink_status_edition import read_designed_packet, write_designed_nums
-
-write_designed_nums([12, 34, 56])
-print(read_designed_packet())
-print(get_downlink_data())
