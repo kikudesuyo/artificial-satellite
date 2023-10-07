e0799e93b7edf426b75c9ba40e1600a82a2830c5
i-spes <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-10-07 16:27:24 +0900

    modify shooting flow

--- src/flow/shooting.py
+++ src/flow/shooting.py
@@ -1,21 +1,17 @@
 from shooting.take_photograph import take_photo
 from shooting.time_relation import is_correct_time
-from helper.file_operation import delete_files_amount, output_raspi_status
+from helper.file_operation import delete_files_amount
 from constant.shooting import ERROR_VALUE
-from constant.status import SHOOTING_COMPLETION, SHOOTING_INTERRUPTION
 
 
-def shooting_flow(time_data):
+def shooting_flow(satellite_time):
   """
   Arg:
-    time_data (list[int]): 4byteの時刻データ[week,hour,minute,half_second]
+    satellite_time (int): 経過時間(秒)
   """
-  output_raspi_status(SHOOTING_INTERRUPTION)
   try:
-    is_correct_time(time_data)
-    satellite_time = time_data[0]*7*24*60*60 + time_data[1]*60*60 + time_data[2]*60 + time_data[3]
+    is_correct_time(satellite_time)
   except:
     satellite_time = ERROR_VALUE
   delete_files_amount( "/img/shooting_img", threshold=2500)
-  take_photo(satellite_time, 3, 2000) #緯度によって強制停止してもいいかもしれない（別のターミナルで）
-  output_raspi_status(SHOOTING_COMPLETION)
+  take_photo(satellite_time, 2, 2000)
