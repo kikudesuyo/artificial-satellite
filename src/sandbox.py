b59950134a450dbd9cc1098e04613ab5e4a4aa79
i-spes <icswse@gmail.com>
2023-10-10 23:10:50 +0900

    make merge_aurora_data

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,16 +1,23 @@
-# from analysis.main import main
-# main()
+import glob
+from natsort import natsorted
+from util import generate_path, delete_files
 
-# from flow.split import split_flow
-# split_flow()
+def merge_aurora_data(first_file_name, num_merged_files):
+  """複数のオーロラデータを結合
+  オーロラデータは1つあたり8byte
+  
+  Args:
+    first_file_name (int): 最初のファイル番号
+    num_merged_files (int): ファイルの結合数
+
+  Return:
+    merged_data (str):
+  """
+  merged_data = ""
+  for file_name in range(first_file_name, first_file_name + num_merged_files):
+    with open(generate_path(f"/data/aurora_data/{file_name}.txt"), "r") as aurora_data_file:
+      aurora_data = aurora_data_file.read()
+    merged_data += aurora_data
+  return merged_data
 
 
-from downlink.uplink_edition import write_uplink_data_to_status
-from downlink.status_edition import read_designed_packet, change_status_file
-
-
-format_array = [34, 234, 7, 3, 23, 24, 64, 43, 53, 56, 34, 41]
-format_array = [34, 234, 7, 3, 32, 5, 53, 14, 53, 23, 23, 24]
-print(read_designed_packet())
-change_status_file(1)
-print(read_designed_packet())
