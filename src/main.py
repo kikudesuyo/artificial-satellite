56cacaae9d9d4d39a19233bc16e036a9de2283e1
i-spes <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-10-11 18:48:45 +0900

    exerience analysis

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
-from format.uart_selection_v2_8 import UartSelection
-instance = UartSelection()
-instance.main()
+# from format.uart_selection_v2_8 import UartSelection
+# instance = UartSelection()
+# instance.main()
+
+from analysis.main import main
+main()
