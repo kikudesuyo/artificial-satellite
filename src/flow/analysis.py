9a79d2d5dd0f636e6a289ff0d67a71ff06dd9000
i-spes <icswse@gmail.com>
2023-10-16 17:56:43 +0900

    rename and  modify delete_files delete_all_files and delte unnecessary parts

--- src/flow/analysis.py
+++ src/flow/analysis.py
@@ -1,9 +1,7 @@
 from analysis.main import main as analysis_main
-from helper.file_operation import delete_files_amount, output_raspi_status
-from constant.status import ANALYSIS_INTERRUPTION, OTHERS_COMPLETION
+from helper.file_operation import delete_files_amount
+
 
 def analysis_flow():
-  output_raspi_status(ANALYSIS_INTERRUPTION)
   delete_files_amount("/data/aurora_data", threshold=1000)
   analysis_main()
-  output_raspi_status(OTHERS_COMPLETION)
