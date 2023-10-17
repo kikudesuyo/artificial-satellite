a70ff935ae439359618b54632ef0e382a4ef2ecd
i-spes <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2023-10-17 19:11:50 +0900

    delete requirements

--- src/sandbox.py
+++ src/sandbox.py
@@ -10,15 +10,5 @@ from flow.split import split_flow
 # split_flow()
 from analysis.main import main
 
-# for _ in range(1, 11):
-#   main()
-
-# delete_files_smaller_than_threshold(5)
-from downlink.status_edition import initialize_status, renew_status_file, read_designed_packet, write_designed_nums
-from helper.file_operation import read_file_contents
-from constant.status import MERGED_AURORA_DATA_NUMBER
-write_designed_nums([12, 453, 53, 43])
-# initialize_status()
-print(read_designed_packet())
-initialize_status()
-print(read_designed_packet())
+for _ in range(1, 11):
+  main()
